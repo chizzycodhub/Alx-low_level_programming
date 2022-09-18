d8f062e5417cab7f28d9bd9cadd0b4a6e589a713
chizzycodhub <chukwudinwobodo01@gmail.com>
2022-09-18 18:32:25 +0000

    Print 0-9 separated by commas and spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Print all possible combination of single-digit number
+ *
+ * Return: 0 On success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
