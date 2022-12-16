c1fe4bea0252cbe333bc714416ba1fd4206a91fa
chizzycodhub <chukwudinwobodo01@gmail.com>
2022-12-16 04:45:24 +0000

    Task 5: print 0 - 14 followed by a new line

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ *
+ *Return: Void
+ */
+void more_numbers(void)
+{
+	int i, j, c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			c = j;
+			if (j > 9)
+			{
+				_putchar('1');
+				c = j % 10;
+			}
+			_putchar(c + '0');
+		}
+		_putchar('\n');
+	}
+}
