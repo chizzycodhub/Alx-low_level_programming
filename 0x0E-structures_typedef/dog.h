c5968800e17b3bb6cce3903f0fb2a9f94f20b21e
chizzycodhub <chukwudinwobodo01@gmail.com>
2023-02-27 16:27:21 +0000

    Task 0 - a new data type describing a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#ifndef DOG_H
+#define DOG_H
+
+/*
+ * File: dog.h
+ * Desc: Define a new data type struct dog.
+ */
+
+
+/**
+ * struct dog -  A new data type describing a dog.
+ *
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: Define a new data type struct dog with the following
+ *              elements:
+ *              name, type = char *
+ *              age, type = float
+ *              owner, type = char *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
