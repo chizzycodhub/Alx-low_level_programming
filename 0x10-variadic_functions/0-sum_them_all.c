65aeba2f3cd53bc9fa05fe1bbd3ac29f77f781ea
chizzycodhub <chukwudinwobodo01@gmail.com>
2023-03-13 09:11:29 +0000

    Task 0 - modified to pass checks

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,9 +13,10 @@ int sum_them_all(const unsigned int n, ...)
 	va_list others;
 	unsigned int i, sum = 0;
 
-	if (n == 0)
-		return (0);
-
+	/**
+	 * if (n == 0)
+	 *		return (0);
+	 */
 	va_start(others, n);
 	for (i = 0; i < n; i++)
 	{
