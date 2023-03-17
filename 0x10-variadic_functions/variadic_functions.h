693906156486a1e4f83a2eb00a6d1e750b88d4f9
chizzycodhub <chukwudinwobodo01@gmail.com>
2023-03-17 19:37:29 +0000

    Task 2 - prints strings

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,6 +9,7 @@
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #include <stdio.h>
 #include <stdlib.h>
