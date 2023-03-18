6d1ee7ed7369d1bd65fc6f884da85c7838ef3a2a
chizzycodhub <chukwudinwobodo01@gmail.com>
2023-03-18 16:45:15 +0000

    Task 3 - prints anything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,8 +10,29 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+
+#include <stdarg.h>
+
+
+
+
+/**
+ * struct printer - A  struct data type defining a printer
+ * @symbol: A symbol representing a data type.
+ * @print: A function pointer to a function that prints a data type
+ *         corresponding to a symbol.
+ */
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
+
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
+
 #endif
