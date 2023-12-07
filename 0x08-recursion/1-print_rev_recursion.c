27a5672d6b4c806539c7157e0f9d133e80735fe3
chizzycodhub <chukwudinwobodo01@gmail.com>
2023-12-07 06:41:57 -0800

    Task 1: a code that prints a given string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a given string in reverse
+ *
+ * @s: String to print.
+ *
+ * Return: Nothing.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
