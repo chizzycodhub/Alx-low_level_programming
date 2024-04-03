00e5df8e23cb810d8047a0242137910ad94dcdc1
chizzycodhub <chukwudinwobodo01@gmail.com>
2024-04-02 22:46:02 -0700

    A function that returns a pointer to a 2D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid - A function that returns a pointer to 2 dimensional
+ *              array of integers.
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: On failure or if either width or height is 0 or negative.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid, w, h;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = (int **) malloc(width * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+
+	for (w = 0; w < width; w++)
+	{
+		grid[w] = (int *) malloc(height * sizeof(int));
+		if (grid[w] == NULL)
+			return (NULL);
+	}
+
+	for (w = 0; w < width; w++)
+	{
+		h = 0;
+		while (h < height)
+		{
+			grid[w][h] = 0;
+			h++;
+		}
+	}
+
+	return (grid);
+}
